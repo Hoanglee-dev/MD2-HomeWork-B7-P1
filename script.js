02ae78024de596e95f3ddc5d59d9cddf201829bf
Hoanglee-dev <levuhoangks@gmail.com>
2024-07-23 16:10:08 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let arr = [1, 7, 5, 9, 2, 6]
+
+/**In ra tổng các phần tử chẵn trong mảng */
+
+  // for (let i = 0; i < arr.length; i++) {
+  //     if ( arr[i] % 2 == 0) {
+  //       console.log(arr[i])
+  //     }
+  //   }
+
+/** In ra tổng các phần tử >5 trong mảng */
+
+  // for (let i = 0; i < arr.length; i++) {
+  //     if ( arr[i] > 5) {
+  //       console.log(arr[i])
+  //     }
+  //   }
+
+/**  In ra tổng các phần tử chia 7 dư 2 trong mảng */  
+  // for (let i = 0; i < arr.length; i++) {
+  //   if ( arr[i] % 7 === 2) {
+  //     console.log(arr[i])
+  //   }
+  // }
+
+/** In ra max của các phần tử chẵn mảng */
+
+  //   let max = arr[0];
+  // for (let i = 0; i < arr.length; i++) {
+  //   if ( arr[i] % 2 === 0) {
+  //     if (arr[i] > max ) {
+  //       max = arr[i]
+  //     }
+  //   } 
+  // }
+  // console.log(max)
+
+/** In ra trung bình các phần tử chia hết cho 5 trong mảng */
+  // for (let i = 0; i < arr.length; i++) {
+  //   if ( arr[i] % 5 === 0 )
+  //     console.log(arr[i])
+  // }
+
+/** Cho người dùng nhập vào 1 số và in ra số lần xuất hiện của số đó trong mảng. */ 
+  // let max = arr[0];
+  // let secondMax = arr[0];
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] > max) {
+  //     max = arr[i];
+  //   } 
+  // }
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] > secondMax && arr[i] < max) {
+  //     secondMax = arr[i];
+  //   }
+  // }
+  // console.log(secondMax);
